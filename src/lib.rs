39707bedb50b60c8eea567b8be4cd308fc31e400
lzpel <18492524+lzpel@users.noreply.github.com>
2025-11-19 23:46:01 +0900

    marching cubes

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+pub mod marching_cubes;
 pub mod region;
 pub mod solid;
+pub mod stl;
 // 「領域」の式ツリー、AIに読みやすいように定義
 pub type Vec2 = glam::DVec2;
 pub type Vec3 = glam::DVec3;
 pub type Float = f64;
+pub type Triangle = [Vec3; 3];
